acc3be5dabfe7bdbf3da2d75ff20bf3c4167153c
Simonmon06 <43972857+Simonmon06@users.noreply.github.com>
2019-01-13 21:33:04 -0500

    Update TaxiServiceDispatchingSystem.java

--- src/TaxiServiceDispatchingSystem.java
+++ src/TaxiServiceDispatchingSystem.java
@@ -1,6 +1,3 @@
-//Zeye Gu
-//101036562
-//Java – Generate random integers in a range. (2017-03-26). https://www.mkyong.com/java/java-generate-random-integers-in-a-range/
 import javafx.animation.KeyFrame;
 import javafx.animation.PauseTransition;
 import javafx.animation.Timeline;
